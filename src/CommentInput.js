961699141847c1061e37f94baf5cc2d3f3dac41d
mistery-li <litujin1123@gmail.com>
2017-09-11 18:42:50 +0800

    CommentInput local username

--- src/CommentInput.js
+++ src/CommentInput.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class CommentInput extends Component {
+    static PropTypes = {
+        onSubmit: PropTypes.func
+    }
     constructor() {
         super()
         this.state = {
             username: '',
             content: ''
         }
+    }
+    componentWillMount() {
+        this._loadUsername()
+    }
+    _loadUsername() {
+        const username = localStorage.getItem('username')
+        if (username) {
+            this.setState({ username })
+        }
+    }
+    componentDidMount() {
+        this.textarea.focus()
+    }
+    _saveUsername(username) {
+        localStorage.setItem('username', username)
+    }
+    handleUsernameBlur(event) {
+        this._saveUsername(event.target.value)
     }
     handleUsernameChange(event) {
         this.setState({
@@ -32,6 +54,7 @@ class CommentInput extends Component {
                     <span className="comment-field-name">用户名：</span>
                     <div className="comment-field-input">
                         <input value={this.state.username}
+                        onBlur={this.handleUsernameBlur.bind(this)}
                         onChange={this.handleUsernameChange.bind(this)}
                         />
                     </div>
@@ -39,7 +62,9 @@ class CommentInput extends Component {
                 <div className="comment-field">
                     <span className="comment-field-name">评论内容：</span>
                     <div className="comment-field-input">
-                        <textarea value={this.state.content}
+                        <textarea
+                        ref={(textarea) => this.textarea = textarea}
+                        value={this.state.content}
                         onChange={this.handleContentChange.bind(this)}
                         />
                     </div>
